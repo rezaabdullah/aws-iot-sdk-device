73002e4f55e987972f3a7ee4411189333019376c
andyloong <andyloong84@gmail.com>
2018-11-10 02:45:18 +0800

    updates

--- thing/index.js
+++ thing/index.js
@@ -1,85 +1,59 @@
-var awsIot = require('aws-iot-device-sdk');
-var levelStore = require('mqtt-level-store');
-var payload = require('./payload.js').payload;
-var levelManager = levelStore('/home/pi/PlusSolar/store');
+const awsIot = require("aws-iot-device-sdk");
+const levelStore = require("mqtt-level-store");
+const payload = require("./payload.js").payload;
+const levelManager = levelStore("./store");
 
-//
-// Replace the values of '<YourUniqueClientIdentifier>' and '<YourCustomEndpoint>'
-// with a unique client identifier and custom host endpoint provided in AWS IoT.
-// NOTE: client identifiers must be unique within your AWS account; if a client attempts 
-// to connect with a client identifier which is already in use, the existing 
-// connection will be terminated.
-//
-var device = awsIot.device({
+const path = "./certs/";
+const device = awsIot.device({
   incomingStore: levelManager.incoming,
   outgoingStore: levelManager.outgoing,
-  keyPath: '/home/pi/PlusSolar/certs/449da88e49-private.pem.key',
-  certPath: '/home/pi/PlusSolar/certs/449da88e49-certificate.pem.crt',
-  caPath: '/home/pi/PlusSolar/certs/AmazonRootCA1.pem',
-  clientId: 'MyRaspberryPi',
-  host: 'a3j1bzslspz95c-ats.iot.ap-southeast-1.amazonaws.com',
+  keyPath: `${path}449da88e49-private.pem.key`,
+  certPath: `${path}449da88e49-certificate.pem.crt`,
+  caPath: `${path}AmazonRootCA1.pem`,
+  clientId: "MyRaspberryPi",
+  host: "a3j1bzslspz95c-ats.iot.ap-southeast-1.amazonaws.com",
   offlineQueueing: false,
-  //	debug: true,
-  port: 8883,
-  //	qos:2,
-  //	clean: false,
-  //	clearSession: false,
-  //	reconnectPeriod: 1000
+  port: 8883
 });
 
 var pubOpts = { qos: 1 };
-
-//
-// Device is an instance returned by mqtt.Client(), see mqtt.js for full
-// documentation.
-//
-
 var interval = null;
-var timeInterval = 10; // 5 seconds
+var timeInterval = 60; // 5 seconds
 
-interval = setInterval(function () {
+interval = setInterval(function() {
   var dat = new Date();
   var pdate = dat.getHours() + ":" + dat.getMinutes() + ":" + dat.getSeconds();
-  console.log('interval date:' + pdate);
-  payload.logger.d = pdate;
+  console.log("interval date:" + pdate);
 
-  //device.subscribe('test/from_aws_console');
-  device.publish('test/from_aws_testing', JSON.stringify(payload), pubOpts);
+  //payload.logger.d = pdate;
+  // payload.logger._id = ``;
+
+  device.publish("publish/reading", JSON.stringify(payload), pubOpts);
 }, timeInterval * 1000);
 
-
-device.on('connect', function () {
-  console.log('connected');
-
-  // if (interval) return;
-
+device.on("connect", function() {
+  console.log("connected");
 });
 
-device.on('message', function (topic, payload) {
-  console.log('receiving message');
+device.on("message", function(topic, payload) {
+  console.log("receiving message");
   //console.log('message', topic, payload.toString());
 });
 
-
-device.on('close', function () {
-  console.log('disconnected');
+device.on("close", function() {
+  console.log("disconnected");
   //console.log('disconnected', arguments);
 });
 
-
-device.on('error', function () {
+device.on("error", function() {
   //console.log('error', arguments);
 });
 
-device.on('reconnect', function () {
-  console.log('reconnect...');
+device.on("reconnect", function() {
+  console.log("reconnect...");
   //console.log('reconnect', arguments);
 });
 
-device.on('timeout', function () {
+device.on("timeout", function() {
   //console.log('timeout', arguments);
 });
-
-
-
-
--- thing/payload.js
+++ thing/payload.js
@@ -1,164 +1,157 @@
-module.exports.payload =
-  {
+module.exports.payload = {
+  logger: {
+    _id: "49546986683135544286507457936321625675700192471156785154-l",
+    locationId: "8574723183",
+    active: true,
+    createdAt: "2018-10-23T06:46:48.782Z",
+    updatedAt: "2018-10-23T06:46:48.782Z",
+    powerAdjustment: 2,
+    ambientTemp: 34,
+    moduleTemp: 45,
+    irrSensor: 18,
+    totalBuildingLoad: 230,
+    activePower: 12,
+    windSpeed: 7
+  },
 
-    logger:
+  inverter: [
     {
-      d: "2018-10-23T06:46:48.782Z",
-      _id: "49546986683135544286507457936321625675700192471156785154-l",
+      _id: "49546986683135544286507457936321625675700192471156785154-i",
+      locationId: "8574723183",
+      dataLoggerId: "647234075",
+      active: true,
+      createdAt: "2018-10-23T06:46:48.782Z",
+      updatedAt: "2018-10-23T06:46:48.782Z",
+      ref: 1,
+      effInverter: 8,
+      acPower: 1,
+      dcPower: 20,
+      pvCurrent: 14,
+      pcVoltage: 19,
+      mpptPower: 6
+    },
+    {
+      _id: "49546986683135544286507457936321625675700192471156785154-i",
+      locationId: "8574723183",
+      dataLoggerId: "647234075",
+      active: true,
+      createdAt: "2018-10-23T06:46:48.782Z",
+      updatedAt: "2018-10-23T06:46:48.782Z",
+      ref: 2,
+      effInverter: 8,
+      acPower: 1,
+      dcPower: 20,
+      pvCurrent: 14,
+      pcVoltage: 19,
+      mpptPower: 6
+    },
+    {
+      _id: "49546986683135544286507457936321625675700192471156785154-i",
+      locationId: "8574723183",
+      dataLoggerId: "647234075",
+      active: true,
+      createdAt: "2018-10-23T06:46:48.782Z",
+      updatedAt: "2018-10-23T06:46:48.782Z",
+      ref: 3,
+      effInverter: 8,
+      acPower: 1,
+      dcPower: 20,
+      pvCurrent: 14,
+      pcVoltage: 19,
+      mpptPower: 6
+    },
+    {
+      _id: "49546986683135544286507457936321625675700192471156785154-i",
+      locationId: "8574723183",
+      dataLoggerId: "647234075",
+      active: true,
+      createdAt: "2018-10-23T06:46:48.782Z",
+      updatedAt: "2018-10-23T06:46:48.782Z",
+      ref: 4,
+      effInverter: 8,
+      acPower: 1,
+      dcPower: 20,
+      pvCurrent: 14,
+      pcVoltage: 19,
+      mpptPower: 6
+    },
+    {
+      _id: "49546986683135544286507457936321625675700192471156785154-i",
+      locationId: "8574723183",
+      dataLoggerId: "647234075",
+      active: true,
+      createdAt: "2018-10-23T06:46:48.782Z",
+      updatedAt: "2018-10-23T06:46:48.782Z",
+      ref: 5,
+      effInverter: 8,
+      acPower: 1,
+      dcPower: 20,
+      pvCurrent: 14,
+      pcVoltage: 19,
+      mpptPower: 6
+    }
+  ],
+
+  powerMeter: [
+    {
+      _id: "49546986683135544286507457936321625675700192471156785154-pm",
       locationId: "8574723183",
       active: true,
       createdAt: "2018-10-23T06:46:48.782Z",
       updatedAt: "2018-10-23T06:46:48.782Z",
-      powerAdjustment: 2,
-      ambientTemp: 34,
-      moduleTemp: 45,
-      irrSensor: 18,
-      totalBuildingLoad: 230,
-      activePower: 12,
-      windSpeed: 7
+      ref: 1,
+      activeEnergy: 10,
+      activePower: 13,
+      voltage: {
+        1: 7,
+        2: 6,
+        3: 8
+      },
+      current: {
+        1: 7,
+        2: 3,
+        3: 1
+      },
+      power: {
+        1: 9,
+        2: 2,
+        3: 7
+      },
+      maxDemand: 16,
+      mDlevel: {
+        1: 23,
+        2: 23
+      }
     },
-
-    inverter: [
-      {
-        _id: "49546986683135544286507457936321625675700192471156785154-i",
-        locationId: "8574723183",
-        dataLoggerId: "647234075",
-        active: true,
-        createdAt: "2018-10-23T06:46:48.782Z",
-        updatedAt: "2018-10-23T06:46:48.782Z",
-        ref: 1,
-        effInverter: 8,
-        acPower: 1,
-        dcPower: 20,
-        pvCurrent: 14,
-        pcVoltage: 19,
-        mpptPower: 6
+    {
+      _id: "49546986683135544286507457936321625675700192471156785154-pm",
+      locationId: "8574723183",
+      active: true,
+      createdAt: "2018-10-23T06:46:48.782Z",
+      updatedAt: "2018-10-23T06:46:48.782Z",
+      ref: 2,
+      activeEnergy: 10,
+      activePower: 13,
+      voltage: {
+        1: 7,
+        2: 6,
+        3: 8
       },
-      {
-        _id: "49546986683135544286507457936321625675700192471156785154-i",
-        locationId: "8574723183",
-        dataLoggerId: "647234075",
-        active: true,
-        createdAt: "2018-10-23T06:46:48.782Z",
-        updatedAt: "2018-10-23T06:46:48.782Z",
-        ref: 2,
-        effInverter: 8,
-        acPower: 1,
-        dcPower: 20,
-        pvCurrent: 14,
-        pcVoltage: 19,
-        mpptPower: 6
+      current: {
+        1: 7,
+        2: 3,
+        3: 1
       },
-      {
-        _id: "49546986683135544286507457936321625675700192471156785154-i",
-        locationId: "8574723183",
-        dataLoggerId: "647234075",
-        active: true,
-        createdAt: "2018-10-23T06:46:48.782Z",
-        updatedAt: "2018-10-23T06:46:48.782Z",
-        ref: 3,
-        effInverter: 8,
-        acPower: 1,
-        dcPower: 20,
-        pvCurrent: 14,
-        pcVoltage: 19,
-        mpptPower: 6
+      power: {
+        1: 9,
+        2: 2,
+        3: 7
       },
-      {
-        _id: "49546986683135544286507457936321625675700192471156785154-i",
-        locationId: "8574723183",
-        dataLoggerId: "647234075",
-        active: true,
-        createdAt: "2018-10-23T06:46:48.782Z",
-        updatedAt: "2018-10-23T06:46:48.782Z",
-        ref: 4,
-        effInverter: 8,
-        acPower: 1,
-        dcPower: 20,
-        pvCurrent: 14,
-        pcVoltage: 19,
-        mpptPower: 6
-      },
-      {
-        _id: "49546986683135544286507457936321625675700192471156785154-i",
-        locationId: "8574723183",
-        dataLoggerId: "647234075",
-        active: true,
-        createdAt: "2018-10-23T06:46:48.782Z",
-        updatedAt: "2018-10-23T06:46:48.782Z",
-        ref: 5,
-        effInverter: 8,
-        acPower: 1,
-        dcPower: 20,
-        pvCurrent: 14,
-        pcVoltage: 19,
-        mpptPower: 6
+      maxDemand: 16,
+      mDlevel: {
+        1: 23,
+        2: 23
       }
-    ],
-
-
-    powerMeter: [
-      {
-        _id: "49546986683135544286507457936321625675700192471156785154-pm",
-        locationId: "8574723183",
-        active: true,
-        createdAt: "2018-10-23T06:46:48.782Z",
-        updatedAt: "2018-10-23T06:46:48.782Z",
-        ref: 1,
-        activeEnergy: 10,
-        activePower: 13,
-        voltage: {
-          1: 7,
-          2: 6,
-          3: 8
-        },
-        current: {
-          1: 7,
-          2: 3,
-          3: 1
-        },
-        power: {
-          1: 9,
-          2: 2,
-          3: 7
-        },
-        maxDemand: 16,
-        mDlevel: {
-          1: 23,
-          2: 23
-        }
-      },
-      {
-        _id: "49546986683135544286507457936321625675700192471156785154-pm",
-        locationId: "8574723183",
-        active: true,
-        createdAt: "2018-10-23T06:46:48.782Z",
-        updatedAt: "2018-10-23T06:46:48.782Z",
-        ref: 2,
-        activeEnergy: 10,
-        activePower: 13,
-        voltage: {
-          1: 7,
-          2: 6,
-          3: 8
-        },
-        current: {
-          1: 7,
-          2: 3,
-          3: 1
-        },
-        power: {
-          1: 9,
-          2: 2,
-          3: 7
-        },
-        maxDemand: 16,
-        mDlevel: {
-          1: 23,
-          2: 23
-        }
-      }
-    ]
-
-
-  };
+    }
+  ]
+};
